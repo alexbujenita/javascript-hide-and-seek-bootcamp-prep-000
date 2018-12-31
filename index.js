65129464f4740047b2bfc3d299384b7a135d0f64
Alex Bujenita <dekc89@gmail.com>
2018-12-31 15:58:01 +0000

    n+1

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+let getFirstSelector = (selector) => document.querySelector(selector)
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  let list = document.querySelectorAll('.ranked-list')
+  list = Array.from(list);
+  list.forEach(ul => {
+    const children = Array.from(ul.children);
+    children.forEach((child) => {
+      child.innerHTML = parseInt(child.innerHTML) + n;
+    })
+  })
+}
